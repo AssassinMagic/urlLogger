b7105ba91e60fee767b29ae8a1597f2506a6a836
Eshaan Jay <jay00015@umn.edu>
2025-01-27 17:11:02 -0600

    add get with tracker to just return

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
+app = FastAPI()
+
+@app.get("/tracker/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
